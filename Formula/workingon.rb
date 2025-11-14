2c7c32bb08a403deb1bf15f43ccdbdc66bdd74fa
Mark Keller <8452750+keller00@users.noreply.github.com>
2025-11-13 23:09:42 -0800

    workingon-1.1.0
    Closes #3.
    
    Signed-off-by: Mark Keller <8452750+keller00@users.noreply.github.com>

--- Formula/workingon.rb
+++ Formula/workingon.rb
@@ -1,19 +1,13 @@
 class Workingon < Formula
   desc "CLI to track what you're working on"
   homepage "https://github.com/keller00/workingon"
-  url "https://github.com/keller00/workingon/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "e0fba6a7c4c2d35614ca1df0e366fb18f1ec64aaa7be4c3476665ee87936f3d4"
+  url "https://github.com/keller00/workingon/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "b881de8be79eca53856e6777496a40404f376a094224f9dd3aad92a95981ae15"
   license "MIT"
 
   livecheck do
     url :stable
     regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
-
-  bottle do
-    root_url "https://github.com/keller00/homebrew-workingon-tap/releases/download/workingon-1.0.0"
-    sha256 cellar: :any,                 arm64_tahoe:  "f673d8398a273be903faa033fde89420dd203e64389c7d0279ccc060e4718a23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "2daf1685a6d88985ef11f7e9bdcd89d7bae07eec69cb4ac63d9e16debf4b9739"
   end
 
   depends_on "rust" => :build
