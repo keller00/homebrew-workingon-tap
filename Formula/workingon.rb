913402f690c92c53607864aadf23f9a120efca5f
Mark Keller <8452750+keller00@users.noreply.github.com>
2025-11-16 05:23:58 +0000

    workingon: add 1.1.1 bottle.

--- Formula/workingon.rb
+++ Formula/workingon.rb
@@ -10,6 +10,12 @@ class Workingon < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/keller00/homebrew-workingon-tap/releases/download/workingon-1.1.1"
+    sha256 cellar: :any,                 arm64_tahoe:  "2edef839a10240c2033ce79303fbfbb42f37b25cfa7250a9c2c6bff50a89816f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0e72bacfdbf154ca03336a56a046acf4172c175f4d48caa4bd520a20be4641d7"
+  end
+
   depends_on "rust" => :build
   depends_on "sqlite"
 
